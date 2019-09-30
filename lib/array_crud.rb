4ab08a384b253f64a2b6090345153a192ad7481a
Jack Staffen <jack.staffen@gmail.com>
2019-09-30 01:03:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,21 +23,23 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  arr.shift
+  arr
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  element = array[index_number]
+  element
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
